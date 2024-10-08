bc0d91a062c9568d88c493eee951b5325438eb0b
aiueolbaP <andresflores200025@gmail.com>
2024-10-08 08:59:06 -0500

    Update Automovil.java

--- Test/src/Automovil.java
+++ Test/src/Automovil.java
@@ -6,8 +6,24 @@ public class Automovil {
     private float consumo = 40;
 
 
-   //para acceder a los valores de atributos privados el metodo debe ser publico
-   //getter y setter
+    //constructores
+
+    public Automovil(){
+
+    }
+
+    public Automovil(String marca){
+        this.marca = marca;
+    }
+
+    public Automovil(String marca, String modelo){
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    //no necesita devolver nada xq es constructor
+
+    //para acceder a los valores de atributos privados el metodo debe ser publico
+    //getter y setter
     public String getMarca() {
         return marca;
     }
@@ -47,6 +63,11 @@ public class Automovil {
     public void setConsumo(float consumo) {
         this.consumo = consumo;
     }
+
+
+
+    // metodos propios y de calculo
+
    /*public void leerMarca(String marca){
        //al atributo marca se le asigna la variable marca
        this.marca = marca;
@@ -59,11 +80,11 @@ public class Automovil {
     */
 
     public String detalleauto(){
-        //este es el metodo
-        //String modelo = "otro";
-        //primero toma la parte interna, por eso toma el valor de otro al estar dentro del proceso
-        //el this diferencia valores dentro o fuera de un producto
-        //this hace que tome primero los datos de la clase y atributos, sin el, toma el valor de las variables que llama
+            //este es el metodo
+            //String modelo = "otro";
+            //primero toma la parte interna, por eso toma el valor de otro al estar dentro del proceso
+            //el this diferencia valores dentro o fuera de un producto
+            //this hace que tome primero los datos de la clase y atributos, sin el, toma el valor de las variables que llama
         /*System.out.println("La marca del auto es: "+ marca);//este es con sout
         System.out.println("auto.modelo = " + this.modelo);//este es con soutv
         System.out.println("auto.color = " + color);
@@ -72,7 +93,7 @@ public class Automovil {
         StringBuilder sb = new StringBuilder();
 
         sb.append("La marca del auto es: "+ this.marca + " \n");//este es con sout
-        //this sirve con private porque esta dentro de la misma clase
+            //this sirve con private porque esta dentro de la misma clase
         sb.append("auto.modelo = " + this.modelo + " \n");//este es con soutv
         sb.append("auto.color = " + this.color + " \n");
         sb.append("auto.cilindraje = " + this.cilindraje + " \n");
@@ -89,13 +110,18 @@ public class Automovil {
         float cm = consumo / (cap*porConsumo);
         return cm;
     }
-// pueden haber 2 metodos con el mismo nombre siempre y cuando sus parametros o procesos sean diferentes
-//eso se llama sobrecarga de métodos
-    //ahi se aplica el principio de polimorfismo, porque el mismo nombre funciona como varios métodos
-    public float capacidadTanque(int cap, int porConsumo){
+        // pueden haber 2 metodos con el mismo nombre siempre y cuando sus parametros o procesos sean diferentes
+        //eso se llama sobrecarga de métodos
+        //ahi se aplica el principio de polimorfismo, porque el mismo nombre funciona como varios métodos
+        public float capacidadTanque(int cap, int porConsumo){
         float cm = consumo / (cap*(porConsumo/100));
         return cm;
     }
+        //el bytecode es intermedio entre programa fuente y programa objeto
+
+
+
+        // Modificacion de metodos nativos
 
 
 
